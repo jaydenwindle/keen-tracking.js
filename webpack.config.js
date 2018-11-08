6ea627cc554aebf0d8b1ab913726393d28a6ec8b
Jayden Windle <jaydenwindle@gmail.com>
2018-11-08 13:57:37 -0800

    added react native separate build

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: `${
-      process.env.TARGET ? `${process.env.TARGET}/` : ''
+      process.env.TARGET ? `${process.env.TARGET}/` : (process.env.REACT_NATIVE ? 'react-native/' : '') 
     }${
       fileName
     }${
